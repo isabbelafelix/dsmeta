e7ddd3c441a8890b136c5c810ba197f4bba7ab4e
Isabela Felix <isabbelafelix@gmail.com>
2022-09-09 20:53:19 -0300

    add twilio sms

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsmeta.controller;
 
 import com.devsuperior.dsmeta.entities.SaleEntity;
 import com.devsuperior.dsmeta.service.SaleService;
+import com.devsuperior.dsmeta.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,11 +20,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<SaleEntity> saleEntityList(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable){
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/notification")
+    public void notifySms(){
+        smsService.sendSms();
     }
 }
